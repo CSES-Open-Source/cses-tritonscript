fcff460d7664dd9b3e6834b235fcba1bbd496f3d
alw017 <alw017@ucsd.edu>
2024-02-20 18:27:32 -0800

    reverting path names and removing debug lines

--- backend/routes/note.ts
+++ backend/routes/note.ts
@@ -1,17 +1,13 @@
 import express from "express";
 import { upload } from "../controllers/note.controller";
 import { notes } from "../controllers/note.controller";
-import { pullTest1 } from "../controllers/note.controller";
 
 const router = express.Router();
 
 // get all notes in order of updatedAt
-router.get("/notes", notes);
+router.get("/", notes);
 
 //post new note
 router.post("/:id", upload);
 
-
-//
-router.get("/")
 export default router;
