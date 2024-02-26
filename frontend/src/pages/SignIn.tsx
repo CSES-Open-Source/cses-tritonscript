b5e5636ffca6a72d8d8adbf16bef27e773b5074b
Scott Semtner <sjsemtner@gmail.com>
2024-02-26 13:50:16 -0800

    Attempt to clean up SignIn styles

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -5,7 +5,7 @@ import { useDispatch, useSelector } from "react-redux";
 // import settings from "../utils/config";
 import { auth } from "../firebase";
 import { signInWithEmailAndPassword } from "firebase/auth";
-import "./SignIn.css";
+import formStyles from "../form.module.css";
 import logoUrl from "../assets/cses-opensource.png";
 import GoogleAuthButton from "../components/GoogleAuthButton/GoogleAuthButton.tsx";
 
@@ -49,26 +49,27 @@ export default function SignIn() {
   }, [currentUser, navigate]);
 
   return (
-    <div className={"sign-in-container"}>
+    <div className={"pageContainer"}>
       {/* I think this section should be part of the navbar component because it's shared */}
-      <div className={"sign-in-footer"}>
+      <div className={""}>
         <p>Dont Have an account?</p>
         <Link to="/signup">
           <span>Sign up</span>
         </Link>
       </div>
 
-      <img src={logoUrl} alt={"CSES Open-Source logo"} />
+      <img className={"csesLogo"} src={logoUrl} alt={"CSES Open-Source logo"} />
 
       <p className={error ? "sign-in-error" : ""}>
-        {error ? error.message || "Something went wrong!" : ""}
+        {error ? error.message || "Something went wrong!" : loading ? "Loading..." : ""}
       </p>
 
-      <div className={"sign-in-form"}>
+      <div className={formStyles.formContainer}>
         <form onSubmit={handleSubmit}>
-          <input type="email" placeholder="Email" id="email" onChange={handleChange} />
-          <input type="password" placeholder="Password" id="password" onChange={handleChange} />
-          <button disabled={loading}>{loading ? "Loading..." : "Sign In"}</button>
+          <input className={formStyles.formInput} type="email" placeholder="Email" id="email" onChange={handleChange} />
+          <input className={formStyles.formInput} type="password" placeholder="Password" id="password"
+                 onChange={handleChange} />
+          <button type="submit" className={formStyles.formSubmit}>Sign In</button>
         </form>
         <p className={"forgot-password"}>Forgot your password?</p>
       </div>
