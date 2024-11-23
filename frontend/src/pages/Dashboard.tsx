d6c5468b6260f7cf2a10cf5d9fd32a3ccfc6ba65
victoriatr6n <108353792+victoriatr6n@users.noreply.github.com>
2024-11-23 14:45:47 -0800

    search bar and filter

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,5 +1,19 @@
+import "../../src/pages/Dashboard.css";
+import filter from '../assets/filter-icon.png';
+
 export default function Dashboard() {
     return (
+    <div>
       <div>dashboard</div>
-    )
-  }
+      <div className="search-features">
+      <input className="search-input"
+        type="text"
+        placeholder="Search..."
+      />
+      <div className="filter">
+        <img className="logo" src={filter} alt="search filter icon" />
+      </div>
+    </div>
+    </div>
+    );
+  };
